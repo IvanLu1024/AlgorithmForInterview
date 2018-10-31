bb7e60fe74e668c170c0091354c998035be356e0
杜厚安 <33805265+DuHouAn@users.noreply.github.com>
2018-10-31 17:54:35 +0800

    查找问题

--- src/com/southeast/code_02_find/Code_242_ValidAnagram.java
+++ src/com/southeast/code_02_find/Code_242_ValidAnagram.java
@@ -67,23 +67,4 @@ public class Code_242_ValidAnagram {
         }
         return true;
     }
-        //使用map统计 s中字符出啊先的次数
-       /* Map<Character,Integer> sMap=new HashMap<>();
-        Map<Character,Integer> tMap=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char sChar=s.charAt(i);
-            int sFreq=sMap.get(s.charAt(i))==null?0:sMap.get(s.charAt(i));
-            sMap.put(sChar,++sFreq);
-            char tChar=t.charAt(i);
-            int tFreq=tMap.get(t.charAt(i))==null?0:tMap.get(t.charAt(i));
-            tMap.put(tChar,++tFreq);
-        }
-        for(int i=0;i<t.length();i++){
-            char tChar=t.charAt(i);
-            if(!sMap.containsKey(tChar) || !(sMap.get(tChar)==tMap.get(tChar))){
-                return false;
-            }
-        }
-        return true;
-        */
 }
